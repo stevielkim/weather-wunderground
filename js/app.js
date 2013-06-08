9df5edd4a899da83ef2e7a48e465fea7ac757cea
StephanieKim <stephanie.kim76@gmail.com>
2013-06-08 13:59:10 -0700

    cleared inputs on focus and added simple css

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
 $(function(){
-    // first user enters data then clicks button to retrieve data
     "use strict";
-
-
-    $('#find').click(function(){
-        var $this = $(this);
-        var location_input = $('#name').val();
+    $('#find').on('click', function(){
+        var location_input = $('#location_name').val();
         function errors(){
             $(".empty_field_error").show();
         }
-        
-        var base_url = "http://api.wunderground.com/api/51c27f40bbfecefc/conditions/q/";
+        var base_url =  "http://api.wunderground.com/api/51c27f40bbfecefc/conditions/q/";
         var j = ".json";
 
-        if(location_input === '' ){
+        if(location_input === ''){
             errors();
         }
         else {
@@ -27,22 +22,23 @@ $(function(){
                  // the DOM
                 success: function(data){
                     // create object literal
-                    var current = {
-                        name_location: data.current_observation.display_location.city,
-                        temp_f: data.current_observation.temp_f,
-                        weather: data.current_observation.weather,
-                        wind_mph: data.current_observation.wind_mph
-                    };
-                    var returned_data = $('#returned_data');
-                    // clears if full
-                    returned_data.empty();
-                    returned_data.html("<h3>Results of " + current.name_location +
-                    "</h3>"  + "<p>Temperature: " + current.temp_f + "</p>" +
-                    "<p>Current Weather: " + current.weather + "</p>" + "<p>Wind Gusts: " +
-                    current.wind_mph + "mph</p>");
+                        var current = {
+                            name_location: data.current_observation.display_location.city,
+                            temp_f: data.current_observation.temp_f,
+                            weather: data.current_observation.weather,
+                            wind_mph: data.current_observation.wind_mph
+                        };
+                        var returned_data = $('#returned_data');
+                        returned_data.html("<h3>Results of " + current.name_location +
+                        "</h3>"  + "<p>Temperature: " + current.temp_f + "</p>" +
+                        "<p>Current Weather: " + current.weather + "</p>" + "<p>Wind Gusts: " +
+                        current.wind_mph + "mph</p>");
+                    $('#location_name').focus(function(){
+                         $(this).val('');
+                         returned_data.detach();
+                    });
                 }
             });
-
         }
 
     });
