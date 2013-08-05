25b5b539b56a37dffacf59666ffc76cb00a0a5df
StephanieKim <stephanie.kim76@gmail.com>
2013-08-04 17:35:17 -0700

    a bit of refactoring & added error when there's no name match

--- js/app.js
+++ js/app.js
@@ -1,45 +1,60 @@
-$(function(){
+(function($) {
     "use strict";
-    $('#find').on('click', function(){
-        var location_input = $('#location_name').val();
-        function errors(){
+    $('#find').on('click', function() {
+
+        var location_input = $('#location_name').val(),
+            base_url =  "http://api.wunderground.com/api/51c27f40bbfecefc/conditions/q/",
+            j = ".json";
+
+        function errors() {
             $(".empty_field_error").show();
         }
-        var base_url =  "http://api.wunderground.com/api/51c27f40bbfecefc/conditions/q/";
-        var j = ".json";
 
-        if(location_input === ''){
+        if (location_input === '') {
             errors();
-        }
-        else {
+        } else {
             $(".empty_field_error").hide();
             // url including base url with api key and query type, datatype is jsonp
-            $.jsonp({
+            $.ajax({
+                type: 'GET',
                 url: base_url + location_input + j + "?callback=?",
+                dataType: 'jsonp',
+                statusCode: {
+                    // 404's etc
+                },
 
                  // Success callback function returning specific data results
                  // from Wunderground API in jsonp format & is inserted in
                  // the DOM
-                success: function(data){
-                    // create object literal
+                success: function(data) {
+
+                    if (data.current_observation !== undefined) {
+
+                        // console.log(data);
+                        // when there is a reponse but no match it returns undefined for data.current_observation
+                        
                         var current = {
                             name_location: data.current_observation.display_location.city,
                             temp_f: data.current_observation.temp_f,
                             weather: data.current_observation.weather,
                             wind_mph: data.current_observation.wind_mph
-                        };
-                        var returned_data = $('#returned_data');
+                        },
+                        returned_data = $('#returned_data');
                         returned_data.html("<h3>Results of " + current.name_location +
                         "</h3>"  + "<p>Temperature: " + current.temp_f + "</p>" +
                         "<p>Current Weather: " + current.weather + "</p>" + "<p>Wind Gusts: " +
                         current.wind_mph + "mph</p>");
-                    $('#location_name').focus(function(){
-                         $(this).val('');
-                         returned_data.detach();
-                    });
+                            
+                        $('#location_name').focus(function() {
+                             $(this).val('');
+                             returned_data.detach();
+                        });
+                    } else {
+                        errors();
+                    }
                 }
             });
         }
 
     });
-});
+})(jQuery);
